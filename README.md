0a99e859f0aa540882394d78f1db8da32cfbe200
Luigi Leung <luigileung@gmail.com>
2018-06-23 18:23:38 -0700

    Add fetching to IPFS hosted test JSON

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ A decentralized Blockchain application
 ## Quick start
 `$ npm install`  
 `$ npm start`  
-Go to http://localhost:3000/
+Go to http://localhost:3000/  
+
+## IPFS notes
+`$ ipfs add FILE`  
+`$ ipfs cat /ipfs/HASH`  
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import {Component} from 'react'
+import fetch from '../lib/fetch'
 import Page from '../components/page'
 
-export default () => (
-  <Page>
-    <h1>SportsChain</h1>
-  </Page>
-)
+const hashNotJson = 'Qmf6xsQkUgVSoEq65wGbsZ4rnpzRxNqEDZPK7DhBVEKRr5'
+const hash0 = 'QmTQqczEPUzyaArjSzpLqH2EGQ2y8Mzc3AY9wR5u1XsgjL'
+
+export default class extends Component {
+  static async getInitialProps({pathname}) {
+    return {
+      blockchain: await fetch(hash0)
+    }
+  }
+  render() {
+    return (
+      <Page>
+        <h1>SportsChain</h1>
+        <div>{JSON.stringify(this.props.blockchain)}</div>
+      </Page>
+    )
+  }
+}
